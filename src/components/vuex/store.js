f1715e2847738b2c711cd72742e389b564266417
Басун Володимир <vbasoon@gmail.com>
2020-09-15 12:59:19 +0300

    store.js filling

--- src/components/vuex/store.js
+++ src/components/vuex/store.js
@@ -1,13 +1,38 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex);
 
 let store = new Vuex.Store({
-   state: {},
-   mutations: {},
-   actions: {},
-   getters: {}
+   state: {
+      products: []
+   },
+   mutations: {
+      SET_PRODUCTS_TO_STATE: (state, products) => {
+         state.products = products
+      }
+   },
+   actions: {
+      GET_PRODUCTS_FROM_API({ commit }) {
+         return axios(('http://localhost:3000/products'), {
+            method: "GET"
+         })
+            .then((products) => {
+               commit('SET_PRODUCTS_TO_STATE', products);
+               return products;
+            })
+            .catch((error) => {
+               console.log(error)
+               return error;
+            })
+      }
+   },
+   getters: {
+      PRODUCTS(state) {
+         return state.products;
+      }
+   }
 });
 
 export default store;

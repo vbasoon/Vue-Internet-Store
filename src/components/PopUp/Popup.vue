eb7d7cfaccc1c27dd52367be73b44b392463123d
Басун Володимир <vbasoon@gmail.com>
2021-03-26 13:47:02 +0200

    popup component

---
+++ src/components/PopUp/Popup.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="popup">
+     <div class="popup__header"></div>
+     <div class="popup__content"></div>
+     <div class="popup__footer"></div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
